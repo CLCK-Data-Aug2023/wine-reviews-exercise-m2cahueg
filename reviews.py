1f0f86a2fa888688ce7354ebd62c799f01d04706
Gregory Cahue <gregory.cahue@gmail.com>
2023-12-22 11:31:45 -0500

    Version 1

--- reviews.py
+++ reviews.py
@@ -1,2 +1,10 @@
-# add your code here
+import pandas
+import zipfile
 
+with zipfile.ZipFile('data/winemag-data-130k-v2.csv.zip', 'r') as x:
+   with x.open('winemag-data-130k-v2.csv') as y:
+       dataframe = pandas.read_csv(y)
+summary = dataframe.groupby('country').agg({'points': ['count', 'mean']})
+summary.columns = ['count', 'points']
+summary['points'] = summary['points'].round(1)
+summary.to_csv('data/reviews-per-country.csv', index=True)
